44d49751477c583d9b2884be10a6da85a39535b7
Shiv <shiviyer55x@gmail.com>
2024-09-25 21:23:35 +0800

    starting to cook, basic structure

--- k2mu/src/App.js
+++ k2mu/src/App.js
@@ -1,17 +1,25 @@
+import React from "react";
 //import logo from './logo.svg';
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+
+// import Main
+import Main from "./Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Knight 2 Meet U
-        </p>
-      </header>
-    </div>
+    // enclose everything within one React.Fragment
+    <React.Fragment>
+      <Main/>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Knight 2 Meet U
+          </p>
+        </header>
+      </div> */}
+    </React.Fragment>
   );
 }
 
---
+++ k2mu/src/Main.js
@@ -0,0 +1,17 @@
+// imports
+import React from "react";
+
+export default class Main extends React.Component {
+    // state
+    state = {
+        currentPage: "home"
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>hi.</h1>
+            </React.Fragment>
+        )
+    }
+}
