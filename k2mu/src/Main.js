5c71457c8e7d319fb8ab6425adeff5c3f766458c
Shiv <shiviyer55x@gmail.com>
2024-09-25 21:51:47 +0800

    added react-bootstrap and bootstrap, testing some styling and format

--- k2mu/src/Main.js
+++ k2mu/src/Main.js
@@ -1,6 +1,17 @@
 // imports
 import React from "react";
 
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// styles
+import "./styles.css";
+
+// import React-Bootstrap components
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+import {Container, Row, Col} from 'react-bootstrap';
+
 export default class Main extends React.Component {
     // state
     state = {
@@ -10,7 +21,16 @@ export default class Main extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <h1>hi.</h1>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="title">hi.</h1>
+                        </Col>
+                        <Col>
+                            <h2 className="title2">bye!</h2>
+                        </Col>
+                    </Row>
+                </Container>
             </React.Fragment>
         )
     }
