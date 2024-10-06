eafd1b4acbfa6f72537cc603e181c0c561703765
Shiv <shiviyer55x@gmail.com>
2024-10-06 11:10:28 +0800

    tournament representation on tournaments page, static values for overall tourneys and user tourneys

--- k2mu/src/pages/Tournaments.js
+++ k2mu/src/pages/Tournaments.js
@@ -1,19 +1,98 @@
-import React from "react";
+import React, {useState} from "react";
 
-import {Container, Button} from "react-bootstrap";
+import {Container, Button, ListGroup, Modal} from "react-bootstrap";
 
 export default function Tournaments () {
+
+    // we will use the state to represent tournaments
+    const [showTournaments, setShowTournaments] = useState(false);
+    const [showUserTournaments, setShowUserTournaments] = useState(false);
     
     const buttonClicked = () => {
         alert("Button was clicked.");
+    }
+
+    // for now, dummy tournament values in a String array
+    const tournaments = [
+        "Tournament 1",
+        "Tournament 2",
+        "Tournament 3",
+        "Tournament 4",
+        "Tournament 5",
+        "Tournament 6",
+        "Tournament 7",
+        "Tournament 8",
+        "Tournament 9",
+        "Tournament 10"
+    ]
+
+    const userTournaments = [
+        "Tournament 1",
+        "Tournament 7"
+    ]
+
+    const handleShowTournaments = () => {
+        setShowTournaments(true);
+    }
+
+    const handleClose = () => {
+        setShowTournaments(false);
+    }
+
+    const handleShowUserTournaments = () => {
+        setShowUserTournaments(true);
+    }
+
+    const handleUserClose = () => {
+        setShowUserTournaments(false);
     }
 
     return (
         <React.Fragment>
             <Container>
                 <h1>Tournaments</h1>
-                <Button onClick={buttonClicked}>View Available Tournaments</Button>
-                <Button variant="info">My Current Tournaments</Button>
+                <Button onClick={handleShowTournaments}>View Available Tournaments</Button>
+                <Button variant="info" onClick={handleShowUserTournaments}>My Current Tournaments</Button>
+
+                {/* Modal will display our tournaments */}
+                <Modal show={showTournaments} onHide={handleClose}>
+                    <Modal.Header>
+                        <Modal.Title>Available Tournaments</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body style={{maxHeight: "400px", overflowY: "scroll"}}>
+                        {/* Map everything from tournaments into a ListGroup for display */}
+                        <ListGroup>
+                            {tournaments.map((tournament, index) => (
+                                <ListGroup.Item key={index}>{tournament}</ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                {/* The same thing but for user tournaments */}
+                <Modal show={showUserTournaments} onHide={handleUserClose}>
+                    <Modal.Header>
+                        <Modal.Title>My Tournaments</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body style={{maxHeight: "400px", overflowY: "scroll"}}>
+                        {/* Map everything from tournaments into a ListGroup for display */}
+                        <ListGroup>
+                            {userTournaments.map((tournament, index) => (
+                                <ListGroup.Item key={index}>{tournament}</ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleUserClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </Container>
         </React.Fragment>
     );
