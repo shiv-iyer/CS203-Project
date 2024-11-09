79822e4c0d31eb79b26169e07c8998559a651fc2
Shiv <shiviyer55x@gmail.com>
2024-11-09 20:44:42 +0800

    functional useState for form fields, and registration and deletion for new player working

--- k2mu/src/pages/User.js
+++ k2mu/src/pages/User.js
@@ -8,10 +8,27 @@ export default function User() {
     const [activeTab, setActiveTab] = useState('login');
     const [error, setError] = useState(null); // Store any error that may occur during the fetch
 
+    const [formFields, setFormFields] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        isAdmin: false
+    });
+
     // Handler for switching between login and register tabs
     const handleTabSelect = (tab) => {
         setActiveTab(tab);
     };
+
+    // handle any change in the form fields
+    const handleInputChange = (e) => {
+        const {name, value, type, checked} = e.target;
+        setFormFields({
+            ...formFields,
+            [name]: type === "checkbox" ? checked : value
+        })
+    }
 
     const viewUsers = async () => {
         try {
@@ -25,12 +42,16 @@ export default function User() {
     }
 
     const registerUser = async () => {
+        
+        const {username, email, password, isAdmin} = formFields;
+        // console.log(formFields);
+        // ROLE_ADMIN vs ROLE_USER
         try {
             const playerData = {
-                "username": "Player6",
-                "password": "Password6",
-                "email": "chooyangh@gmail.com",
-                "authorities": "ROLE_ADMIN",
+                "username": username,
+                "password": password,
+                "email": email,
+                "authorities": isAdmin ? "ROLE_ADMIN" : "ROLE_USER",
                 "globalEloRating": 2666
             };
 
@@ -42,6 +63,16 @@ export default function User() {
     
             alert("Player created successfully!");
             console.log('Response:', response.data);
+
+            // clear form fields
+            setFormFields({
+                username: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                isAdmin: false,
+            });
+
         } catch (error) {
             // handle error in posting
             console.error("Error creating player:", error);
@@ -52,11 +83,11 @@ export default function User() {
     const deleteUser = async () => {
         try {
 
-            const username = 'Player6';
-            const password = 'Password6';
+            const username = 'Player77';
+            const password = 'Password77';
             const encodedCredentials = btoa(`${username}:${password}`);  // Encode the credentials in Base64
 
-            const response = await axios.delete('http://localhost:8080/players/Player6', {
+            const response = await axios.delete('http://localhost:8080/players/Player77', {
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': `Basic ${encodedCredentials}`
@@ -132,17 +163,56 @@ export default function User() {
                             <Form>
                                 <Form.Group className="mb-3" controlId="registerUsername">
                                     <Form.Label>Username</Form.Label>
-                                    <Form.Control placeholder="Enter Username"></Form.Control>
+                                    <Form.Control
+                                        name="username"
+                                        placeholder="Enter Username"
+                                        value={formFields.username}
+                                        onChange={handleInputChange}
+                                    />
                                 </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="registerEmail">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        name="email"
+                                        placeholder="Enter Email"
+                                        value={formFields.email}
+                                        onChange={handleInputChange}
+                                    />
+                                </Form.Group>
+
 
                                 <Form.Group className="mb-3" controlId="registerPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Enter Password"></Form.Control>
+                                    <Form.Control
+                                        name="password"
+                                        type="password"
+                                        placeholder="Enter Password"
+                                        value={formFields.password}
+                                        onChange={handleInputChange}
+                                    />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="registerConfirmPassword">
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Confirm Password"></Form.Control>
+                                    <Form.Control
+                                        name="confirmPassword"
+                                        type="password"
+                                        placeholder="Confirm Password"
+                                        value={formFields.confirmPassword}
+                                        onChange={handleInputChange}
+                                    />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3 custom-switch" controlId="registerRole">
+                                    <Form.Check
+                                        type="switch"
+                                        id="role"
+                                        label="Admin?"
+                                        name="isAdmin"
+                                        checked={formFields.isAdmin}
+                                        onChange={handleInputChange}
+                                    />
                                 </Form.Group>
 
                                 <Button variant="primary" onClick={registerUser}>Register</Button>
